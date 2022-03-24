013f77f852a8dd925fc7ca9c144e8d0bc2a3de28
Telmo Briones <Telmo.Briones22@gmail.com>
2022-03-24 19:05:20 +0100

    Datu basea sortuta

--- 3marroiaWebZerbitzuak/src/main/java/service/WebServiceLogic.java
+++ 3marroiaWebZerbitzuak/src/main/java/service/WebServiceLogic.java
@@ -19,27 +19,27 @@ public class WebServiceLogic implements WebServiceLogicInterface {
 	public WebServiceLogic() {
 		Flight_db_Access flightsDB = new Flight_db_Access();
 
-		Flight f1 = new Flight("F1", "Donostia", "Bilbo");
-		String fc1 = f1.getFlightCode();
-
-		Flight f2 = new Flight("F2", "Bilbo", "Donostia");
-		String fc2 = f2.getFlightCode();
-
-		flightsDB.storeFlight(fc1, f1.getDepartingCity(), f1.getArrivingCity());
-
-		flightsDB.addConcreteFlight(fc1, fc1 + "CF1", newDate(2022, 1, 22), 1, 2, 3, "12:00");
-		flightsDB.addConcreteFlight(fc1, fc1 + "CF2", newDate(2022, 1, 23), 3, 0, 3, "12:00");
-		flightsDB.addConcreteFlight(fc1, fc1 + "CF3", newDate(2022, 1, 23), 1, 2, 2, "13:00");
-		flightsDB.addConcreteFlight(fc1, fc1 + "CF4", newDate(2022, 1, 23), 3, 3, 0, "14:00");
-		flightsDB.addConcreteFlight(fc1, fc1 + "CF5", newDate(2022, 1, 23), 3, 3, 0, "15:00");
-		flightsDB.addConcreteFlight(fc1, fc1 + "CF6", newDate(2022, 1, 23), 3, 3, 0, "16:00");
-		flightsDB.addConcreteFlight(fc1, fc1 + "CF7", newDate(2022, 1, 23), 3, 3, 0, "17:00");
-
-		flightsDB.storeFlight(fc2, f2.getDepartingCity(), f2.getArrivingCity());
-
-		flightsDB.addConcreteFlight(fc2, fc2 + "CF1", newDate(2022, 1, 21), 3, 3, 0, "12:00");
-		flightsDB.addConcreteFlight(fc2, fc2 + "CF2", newDate(2022, 1, 22), 3, 3, 0, "12:00");
-		flightsDB.addConcreteFlight(fc2, fc2 + "CF3", newDate(2022, 1, 23), 3, 3, 0, "12:00");
+//		Flight f1 = new Flight("F1", "Donostia", "Bilbo");
+//		String fc1 = f1.getFlightCode();
+//
+//		Flight f2 = new Flight("F2", "Bilbo", "Donostia");
+//		String fc2 = f2.getFlightCode();
+//
+//		flightsDB.storeFlight(fc1, f1.getDepartingCity(), f1.getArrivingCity());
+//
+//		flightsDB.addConcreteFlight(fc1, fc1 + "CF1", newDate(2022, 1, 22), 1, 2, 3, "12:00");
+//		flightsDB.addConcreteFlight(fc1, fc1 + "CF2", newDate(2022, 1, 23), 3, 0, 3, "12:00");
+//		flightsDB.addConcreteFlight(fc1, fc1 + "CF3", newDate(2022, 1, 23), 1, 2, 2, "13:00");
+//		flightsDB.addConcreteFlight(fc1, fc1 + "CF4", newDate(2022, 1, 23), 3, 3, 0, "14:00");
+//		flightsDB.addConcreteFlight(fc1, fc1 + "CF5", newDate(2022, 1, 23), 3, 3, 0, "15:00");
+//		flightsDB.addConcreteFlight(fc1, fc1 + "CF6", newDate(2022, 1, 23), 3, 3, 0, "16:00");
+//		flightsDB.addConcreteFlight(fc1, fc1 + "CF7", newDate(2022, 1, 23), 3, 3, 0, "17:00");
+//
+//		flightsDB.storeFlight(fc2, f2.getDepartingCity(), f2.getArrivingCity());
+//
+//		flightsDB.addConcreteFlight(fc2, fc2 + "CF1", newDate(2022, 1, 21), 3, 3, 0, "12:00");
+//		flightsDB.addConcreteFlight(fc2, fc2 + "CF2", newDate(2022, 1, 22), 3, 3, 0, "12:00");
+//		flightsDB.addConcreteFlight(fc2, fc2 + "CF3", newDate(2022, 1, 23), 3, 3, 0, "12:00");
 
 		flightsDB.close();
 	}
